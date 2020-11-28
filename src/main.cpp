7629b7ebe73264a1ed57e06b814eddfad73eec4c
Igor Chernov <cher.igor@outlook.com>
2020-11-29 00:38:03 +0300

    Описал приветственное сообщение
    - Шапка
    - Текущий уровень
    - Произведение чисел
    - Сумма чисел
    - Приглашение для ввода "кода"

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int a = 4;
+int b = 3;
+int c = 2;
+
+int sum = a + b + c;
+int product = a * b * c;
+
 int main() {
+    cout << "======Triple X game!======" << endl;
+    cout << "Уровень: <1>" << endl;
+    cout << "Введите секретный код чтобы завершить уровень." << endl;
+    cout << "+ Код состоит из <3> чисел" << endl;
+    cout << "+ Произведение всех чисел должно быть равным " << product << endl;
+    cout << "+ Сумма всех чисел должна быть равна " << sum << endl;
+    cout << endl;
+    cout << "Код: ";
     return 0;
 }
